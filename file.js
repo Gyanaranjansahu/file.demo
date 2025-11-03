6a9b72beeb10d7d75b66a68bc13bb2abdeeb98b1
gyana <gyana9619@gmail.com>
2025-11-03 19:35:58 +0530

    change

---
+++ file.js
@@ -0,0 +1,19 @@
+let button=document.querySelector(".about")
+let menu=document.querySelectorAll(".container-info")
+let bar=document.querySelector(".info-bar")
+
+let con=true;
+button.addEventListener("click",()=>{
+    menu.forEach((val)=>{
+        if(con=true){
+            val.after(bar);
+            console.log("true")
+            con=false;
+        }
+        else{
+            
+           button.after(val);
+           console.log("false")
+        }
+    })
+})
